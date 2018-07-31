67f1669e8ec9210a6697219c58f4d494ebe0ac6c
chenhz <chenhangzhong@utry.cn>
2018-07-31 18:28:20 +0800

    read file line by line

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"github.com/kubernetes/staging/src/k8s.io/apimachinery/pkg/util/json"
+	"io"
+)
+
 type Label struct {
 	ClusterName		string `json:"cluster_name"`
 	ContainerName	string `json:"container_name"`
@@ -17,13 +25,46 @@ type Resource struct{
 
 type Log struct {
 	InsertId 		string 		`json:"insertId"`
+	Labels			string		`json:"labels"`
 	LogName 		string 		`json:"logName"`
+	ReceiveTimeStamp	string	`json:"receiveTimeStamp"`
 	Resource 		Resource 	`json:"resources"`
 	Severity 		string 		`json:"severity"`
 	TextPayLoad 	string 		`json:"textPayload"`
 	TimeStamp 		string 		`json:"timestamp"`
 }
 
+type Record struct {
+	countN, minCostTime, maxCostTime int
+	costTime []int
+}
+
+var m				map[string]Record
+var redirectCount	int
+
 func main() {
-	
+	jsonFile, err := os.Open("D:\\GoProject\\src\\github.com\\HelloChenHZ\\logAnalysis\\S0.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	read := bufio.NewReader(jsonFile)
+	countN := 0
+	var log Log
+	for {
+		line, isPrefix, err := read.ReadLine()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		json.Unmarshal(line, &log)
+		fmt.Printf("%v th log  is %v and isPrefix is %v\n", countN, log.TextPayLoad, isPrefix)
+		countN ++
+	}
 }
